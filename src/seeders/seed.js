f860866cf8ce41ac0ea9bf20a8373589c5d3f746
Sunthorn Ruckani <sunthorn.rk@gmail.com>
2025-02-23 23:55:02 +0700

    Update Seed Data

--- src/seeders/seed.js
+++ src/seeders/seed.js
@@ -1,6 +1,8 @@
-const {User, Currency} = require("../models");
+const {sequelize, User, Currency} = require("../models");
 
 async function seedDatabase() {
+  await sequelize.sync({force: true}); // ลบ DB เดิมและสร้างใหม่
+
   await User.create({
     username: "testuser",
     email: "test@example.com",
@@ -10,6 +12,10 @@ async function seedDatabase() {
   await Currency.bulkCreate([
     {currency_code: "BTC", currency_name: "Bitcoin"},
     {currency_code: "ETH", currency_name: "Ethereum"},
+    {currency_code: "XRP", currency_name: "Ripple"},
+    {currency_code: "DOGE", currency_name: "Dogecoin"},
+    {currency_code: "THB", currency_name: "Thai Baht"},
+    {currency_code: "USD", currency_name: "US Dollar"},
   ]);
 
   console.log("Seeding completed!");
