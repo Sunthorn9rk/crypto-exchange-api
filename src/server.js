a73de1caf1e1023d0d01d19bb0cd42cee1d02884
Sunthorn Ruckani <sunthorn.rk@gmail.com>
2025-02-24 01:57:03 +0700

    Update Server
    Update Path Route Model

--- src/server.js
+++ src/server.js
@@ -3,17 +3,17 @@ const app = express();
 const bodyParser = require("body-parser");
 const usersRoutes = require("./routes/users.routes");
 const walletsRoutes = require("./routes/wallets.routes");
-// const transactionsRoutes = require("./routes/transactions");
-// const ordersRoutes = require("./routes/orders");
-// const externalTransfersRoutes = require("./routes/externalTransfers");
+const transactionsRoutes = require("./routes/transactions.routes");
+const ordersRoutes = require("./routes/orders.routes");
+const externalTransfersRoutes = require("./routes/externaltransfers.routes");
 
 app.use(bodyParser.json());
 
 app.use("/api/users", usersRoutes);
 app.use("/api/wallets", walletsRoutes);
-// app.use("/api/transactions", transactionsRoutes);
-// app.use("/api/orders", ordersRoutes);
-// app.use("/api/external-transfers", externalTransfersRoutes);
+app.use("/api/transactions", transactionsRoutes);
+app.use("/api/orders", ordersRoutes);
+app.use("/api/external-transfers", externalTransfersRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
