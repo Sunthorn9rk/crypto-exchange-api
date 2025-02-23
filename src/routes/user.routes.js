9b4f513c5d6731fa1d892de2d60ae4b53389aff8
Sunthorn Ruckani <sunthorn.rk@gmail.com>
2025-02-24 00:37:54 +0700

    Add More Route for User Table like getbyid,createuser,updateuser,deleteuser

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/user.controller");
+const UserController = require("../controllers/user.controller");
 
-router.get("/users", userController.getAllUsers);
+// GET: ดึงข้อมูลผู้ใช้ทั้งหมด
+router.get("/users", UserController.getAllUsers);
+
+// GET: ดึงข้อมูลผู้ใช้ตาม ID
+router.get("/:id", UserController.getUserById);
+
+// POST: สร้างผู้ใช้ใหม่
+router.post("/", UserController.createUser);
+
+// PUT: แก้ไขข้อมูลผู้ใช้
+router.put("/:id", UserController.updateUser);
+
+// DELETE: ลบผู้ใช้
+router.delete("/:id", UserController.deleteUser);
 
 module.exports = router;
