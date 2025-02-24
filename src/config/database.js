2b5f8b3c528253c459affa23ed9397efd1bb3cae
Sunthorn Ruckani <sunthorn.rk@gmail.com>
2025-02-24 11:05:51 +0700

    update env

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "./database.sqlite", // ใช้ SQLite ใน local  development เก็บฐานข้อมูลในไฟล์ database.sqlite
-  logging: false, // ปิดการ log query
+  storage: process.env.DB_STORAGE || "./database.sqlite", // ใช้ SQLite ใน local  development เก็บฐานข้อมูลในไฟล์ database.sqlite
+  logging: process.env.DB_LOGGING === "true", // ปิดการ log query
 });
 
 module.exports = sequelize;
