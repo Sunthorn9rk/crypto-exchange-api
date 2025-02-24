ee61920d73fa0423f944470903b0eb59142edc08
Sunthorn Ruckani <sunthorn.rk@gmail.com>
2025-02-24 10:54:30 +0700

    fix database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 const {Sequelize} = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "./database.sqlite", // ใช้ SQLite ใน local development
+  storage: "./database.sqlite", // ใช้ SQLite ใน local  development เก็บฐานข้อมูลในไฟล์ database.sqlite
   logging: false, // ปิดการ log query
 });
 
